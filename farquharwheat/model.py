67e3d6f04cf443229f114f36d0cc651441684a76
Romain Barillot <22076811+rbarillot@users.noreply.github.com>
2015-03-20 10:26:42 +0000

    - Change variable name 'leaf' to 'organ' in order to be more generic - Add Ts (organ temperature) and gs (stomatal conductance) to outputs - Modify the expression of ra (aerodynamic resistance) in order to be clearer and more related the literature (link with physical constants and basic equations from Monteith more explicit) - Modify the expression of rbh (boundary layer resistance) for the same reasons than above + introduce a distinct equation for lamina and stem organs

--- farquharwheat/model.py
+++ farquharwheat/model.py
@@ -36,7 +36,7 @@ class PhotosynthesisModel(object):
     #: Derived from Braune et al. (2009):
     #:
     #:     * S_Na: slope of the relation between Na and the parameters (umol g-1 s-1)
-    #:     * Na_min: minimum amount of leaf nitrogen below which photosynthesis rate is zero (g (N) m-2 leaf)
+    #:     * Na_min: minimum amount of nitrogen below which photosynthesis rate is zero (g (N) m-2)
     #:     * Gamma_Na1 and Gamma_Na2: parameters of ALPHA dependance to Na (mol mol-1 and m2 g-1 respectively)
     #:     * delta1 and delta2: parameters of m (scaling factor of gs) dependance to Na (m2 g-1 and dimensionless respectively)
     PARAM_N = {'S_Na': {'Vc_max25': 63.2, 'Jmax25': 151, 'TPU25': 9.25, 'Rdark25': 0.493}, 'Na_min': {'Vc_max25': 0.198, 'Jmax25': 0.225, 'TPU25': 0.229, 'Rdark25': 0.118},
@@ -45,17 +45,20 @@ class PhotosynthesisModel(object):
     GSMIN = 0.05            #: Stomatal conductance parameter: Minimum gs, measured in the dark (mol m-2 s-1). Braune et al. (2009).
     GB = 3.5                #: Stomatal conductance parameter: Boundary layer conductance (mol m-2 s-1). Muller et al., (2005)
 
-    SIGMA = 5.6704E-8       #: Physical parameter: Stefan-Bolzmann constant (W-2 K-4)
+    A = 2.5                 #: Physical parameter: Attenuation coefficient of wind within a wheat canopy. From Campbell and Norman (1998), second edition. Can also be estimaed by: A = sqrt((0.2*LAI*h)/sqrt((4*width*h)/(pi*LAI))
+    GAMMA = 66E-3           #: Physical parameter: Psychrometric constant (KPa K-1). Mean value
     I0 = 1370               #: Physical parameter: Extraterrestrial solar radiation (W m-2)
+    K =0.40                 #: Physical parameter: Von Kármán's constant (dimensionless)
     LAMBDA = 2260E3         #: Physical parameter: Latent heat for vaporisation of water (J kg-1)
     RHOCP = 1256            #: Physical parameter: Volumetric heat capacity of air (J m-3 K-1)
-    GAMMA = 66E-3           #: Physical parameter: Psychrometric constant (KPa K-1). Mean value
-    A = 2.5                 #: Physical parameter: Attenuation coefficient of wind within a wheat canopy. From Campbell and Norman (1998), second edition. Can also be estimaed by: A = sqrt((0.2*LAI*h)/sqrt((4*width*h)/(pi*LAI))
+    SIGMA = 5.6704E-8       #: Physical parameter: Stefan-Bolzmann constant (W-2 K-4)
+    ZR = 2                  #: Physical parameter: Height above canopy at which reference wind (Ur) is measured (m)
+
     R = 8.3144              #: Physical parameter: Gas constant (J mol-1 K-1)
     PATM = 1.01325E5        #: Physical parameter: Atmospheric pressure (Pa)
 
-    FR = 0.1                #: Physical parameter: Leaf radiation reflectance
-    FT = 0.1                #: Physical parameter: Leaf radiation transmittance
+    FR = 0.1                #: Physical parameter: radiation reflectance
+    FT = 0.1                #: Physical parameter: radiation transmittance
 
     #: Temperature dependance of photosynthetic parameters
     #:
@@ -70,33 +73,42 @@ class PhotosynthesisModel(object):
                   'Tref': 298.15, 'R': 8.3145E-03}
 
     @classmethod
-    def _leaf_temperature(cls, leaf_width, z, H, wind0, PAR, gs, Ta, Tleaf, RH):
+    def _organ_temperature(cls, w, z, Zh, Ur, PAR, gs, Ta, Torg, RH, organ_name):
         """
-        Energy balance for the estimation of leaf temperature
-            - leaf_width (m)
-            - z: height of leaf from soil (m)
-            - H: canopy height (m)
-            - Wind0: wind at the top of the canopy (m s-1)
+        Energy balance for the estimation of organ temperature
+            - w: organ characteristic dimension (m) to be considered for heat transfer through forced convection (by wind).
+                 For a leaf: its width (more related to wind direction than length), for cylindric stem elements: diameter.
+            - z: organ height from soil (m)
+            - Zh: canopy height (m)
+            - Ur: wind at the reference height (zr) (m s-1), e.g. top of the canopy + 2m (in the case of wheat, Ur can be approximated as the wind speed at 2m from soil)
             - PAR (umol m-2 s-1)
             - gs: stomatal conductance (mol m-2 s-1)
             - Ta: air temperature (degree C)
-            - Tleaf: leaf temperature (degree C). Tleaf = Ta at the first iteration of the numeric resolution
+            - Torg: organ temperature (degree C). Torg = Ta at the first iteration of the numeric resolution
             - RH: Relative humidity (decimal fraction)
 
-        :class:`_leaf_temperature` should NOT be INSTANTIATE directly.
+        :class:`_org_temperature` should NOT be INSTANTIATE directly.
         """
 
-        # Wind speed (m s-1)
-        wind = wind0 * exp(cls.A*(z/H -1))                      # From Campbell and Norman (1998), second edition. z: organ height, H: height of the canopy
+        d = 0.7*Zh                                          # Zero plane displacement height (m)
+        Zo = 0.1*Zh                                         # Roughness length (m)
 
-        # Boundary layer restistance to heat (s m-1)
-        rbh = 100*sqrt(leaf_width/wind)
+        # Wind speed
+        u_star = (Ur * cls.K) / log((cls.ZR - d)/Zo)        # Friction velocity (m s-1)
+        Uh = (u_star/cls.K) * log((Zh-d)/Zo)                # Wind speed at the top of canopy (m s-1)
+        u = Uh * exp(cls.A*(z/Zh -1))                       # Wind speed at organ height (m s-1), from Campbell and Norman (1998), second edition.
+
+        # Boundary layer resistance to heat (s m-1). See Finnigan J, Raupach M. 1987 and Monteith JL. 1973 for basic equations.
+        if organ_name == 'Lamina':
+            rbh = 154*sqrt(w/u)                             # Case of horizontal planes submitted to forced convection
+        else:
+            rbh = w / (1.2E-5 * ((u*w)/1.5E-5)**0.47)       # Case of vertical cylinders submitted to forced convection
 
         # Turbulence resistance to heat (s m-1)
-        rt = (0.74 * log(((2 - 0.7*H) / (0.1*H))**2)) / (0.16*wind)
+        ra = 1/(cls.K**2 * Ur) * (log((cls.ZR - d)/Zo))**2  # Aerodynamic resistance integrated from zr to z0 + d
 
         # Net absorbed radiation Rn (PAR and NIR, J m-2 s-1)
-        Iabs = (PAR*(1-cls.FR-cls.FT))/(0.55*4.55)          # Global absorbed radiation by leaf (J m-2 s-1). TODO: relation a verifier
+        Iabs = (PAR*(1-cls.FR-cls.FT))/(0.55*4.55)          # Global absorbed radiation by organ (J m-2 s-1). TODO: relation a verifier
         es_Ta = 0.611 * exp((17.4*Ta)/(239+Ta))             # Saturated vapour pressure of the air (kPa), Ta in degree Celsius
         V = RH * es_Ta                                      # Vapour pressure of the air (kPa)
         fvap = 0.56 - 0.079*sqrt(10*V)                      # Fraction of vapour pressure
@@ -104,27 +116,27 @@ class PhotosynthesisModel(object):
         tau = Iabs/cls.I0                                   # Atmospheric transmissivity (dimensionless)
         fclear = 0.1 + 0.9*max(0, min(1, (tau-0.2)/0.5))    # Fraction sky clearness
 
-        Rn = Iabs - cls.SIGMA * (Tleaf+273)**4*fvap*fclear
+        Rn = Iabs - cls.SIGMA * (Torg+273)**4*fvap*fclear
 
         # Transpiration (mm s-1), Penman-Monteith
-        if Tleaf == Ta:
+        if Torg == Ta:
             Ta_K = Ta + 273.15                              # Ta in kelvin
             s = ((17.4*239)/(Ta_K + 239)**2)*es_Ta          # Slope of the curve relating saturation vapour pressure to temperature (kPa K-1)
         else:
-            es_Tl = 0.611 * exp((17.4*Tleaf)/(239+Tleaf))   # Saturated vapour pressure at leaf (kPa), Tleaf in degree Celsius
-            Tleaf_K, Ta_K = Tleaf + 273.15, Ta + 273.15     # Temperatures in kelvin
-            s = (es_Tl - es_Ta)/(Tleaf_K - Ta_K)            # Slope of the curve relating saturation vapour pressure to temperature (kPa K-1)
+            es_Tl = 0.611 * exp((17.4*Torg)/(239+Torg))     # Saturated vapour pressure at organ level (kPa), Torg in degree Celsius
+            Torg_K, Ta_K = Torg + 273.15, Ta + 273.15       # Temperatures in kelvin
+            s = (es_Tl - es_Ta)/(Torg - Ta_K)               # Slope of the curve relating saturation vapour pressure to temperature (kPa K-1)
 
         VPDa = es_Ta - V
         rbw = 0.96 * rbh                                    # Boundary layer resistance for water (s m-1)
-        gsw = (1.6*gs * cls.R * (Tleaf+273.15)) / cls.PATM  # Stomatal conductance to water (m s-1). 1.6 convert gs_CO2 in gs_water. Relation given by A. Tuzet (2003)
+        gsw = (1.6*gs * cls.R * (Torg+273.15)) / cls.PATM   # Stomatal conductance to water (m s-1). 1.6 convert gs_CO2 in gs_water. Relation given by A. Tuzet (2003)
         rswp = 1/gsw                                        # Stomatal resistance for water (s m-1)
 
-        Tr = max(0, (s * Rn + (cls.RHOCP * VPDa)/(rbh + rt)) / (cls.LAMBDA * (s + cls.GAMMA*((rbw + rt + rswp)/(rbh + rt)))))
+        Tr = max(0, (s * Rn + (cls.RHOCP * VPDa)/(rbh + ra)) / (cls.LAMBDA * (s + cls.GAMMA*((rbw + ra + rswp)/(rbh + ra)))))
 
-        # Leaf temperature
-        Tleaf = Ta + ((rbh + rt) * (Rn - cls.LAMBDA*Tr)) / cls.RHOCP
-        return Tleaf, Tr
+        # Organ temperature
+        Torg = Ta + ((rbh + ra) * (Rn - cls.LAMBDA*Tr)) / cls.RHOCP
+        return Torg, Tr
 
     @classmethod
     def _stomatal_conductance(cls, Ag, An, Na, ambient_CO2, RH):
@@ -132,13 +144,13 @@ class PhotosynthesisModel(object):
         BWB model of stomatal conductance
             - Ag: global assimilation (umol m-2 s-1)
             - An: net assimilation (umol m-2 s-1)
-            - Na: nitrogen content of leaf (g m-2)
+            - Na: nitrogen content of organ (g m-2)
             - ambient_CO2: Air CO2 (umol mol-1)
             - RH: Relative humidity (decimal fraction)
         :class:`_stomatal_conductance` should NOT be INSTANTIATE directly.
         """
 
-        Cs = ambient_CO2 - An *(1.37/(cls.GB))                   # CO2 concentration at leaf surface (umol mol-1 or Pa). From Prieto et al. (2012). GB in mol m-2 s-1
+        Cs = ambient_CO2 - An *(1.37/(cls.GB))                   # CO2 concentration at org surface (umol mol-1 or Pa). From Prieto et al. (2012). GB in mol m-2 s-1
         m = cls.PARAM_N['delta1'] * Na**cls.PARAM_N['delta2']    # Scaling factor dependance to Na (dimensionless). This focntion is maintained although I'm not sure that it should be taken into account
         gs = (cls.GSMIN + m*((Ag*RH)/(Cs)))                      # Stomatal conductance (mol m-2 s-1), from Braune et al. (2009), Muller et al. (2005): using Ag rather than An. Would be better with a function of VPD and with (Ci-GAMMA) instead of Cs.
         return gs
@@ -149,7 +161,7 @@ class PhotosynthesisModel(object):
         Photosynthetic parameters relation to temperature
             - pname: name of parameter
             - p25: parameter value at 25 degree C
-            - T: leaf temperature (degree C)
+            - T: organ temperature (degree C)
         :class:`_f_temperature` should NOT be INSTANTIATE directly.
         """
         Tk = T + 273.15
@@ -171,37 +183,37 @@ class PhotosynthesisModel(object):
         return p
 
     @classmethod
-    def _photosynthesis(cls, PAR, Na, Tleaf, Ci):
+    def _photosynthesis(cls, PAR, Na, Torg, Ci):
         """
         Estimation of C02 assimilation. In this version, most of parameters are derived from Braune et al. (2009) on barley
-            - PAR: PAR intercepted by leaf (umol m-2 s-1)
-            - Na: nitrogen content of leaf (g m-2)
-            - Tleaf: leaf temperature (degree C)
+            - PAR: PAR intercepted by organ (umol m-2 s-1)
+            - Na: nitrogen content of organ (g m-2)
+            - Torf: organ temperature (degree C)
             - Ci: internal CO2 (umol mol-1), by default = 0.7*CO2air
         :class:`_photosynthesis` should NOT be INSTANTIATE directly.
         """
 
         ### # RuBisCO-limited carboxylation rate ###
         # RuBisCO parameters dependance to temperature
-        Kc = cls._f_temperature('Kc', cls.KC25, Tleaf)
-        Ko = cls._f_temperature('Ko', cls.KO25, Tleaf)
-        Gamma = cls._f_temperature('Gamma', cls.GAMMA25, Tleaf)
+        Kc = cls._f_temperature('Kc', cls.KC25, Torg)
+        Ko = cls._f_temperature('Ko', cls.KO25, Torg)
+        Gamma = cls._f_temperature('Gamma', cls.GAMMA25, Torg)
 
         # Vcmax
         Vc_max25 = 84.965 * (Na - 0.17)                                                     # Relation between Vc_max25 and Na
-        Vc_max = cls._f_temperature ('Vc_max', Vc_max25, Tleaf)                             # Relation between Vc_max and temperature
+        Vc_max = cls._f_temperature ('Vc_max', Vc_max25, Torg)                              # Relation between Vc_max and temperature
         Ac = (Vc_max * (Ci-Gamma)) / (Ci + Kc * (1 + cls.O/Ko))                             # Rate of assimilation under Vc_max limitation
         ### RuBP regeneration-limited carboxylation rate via electron transport ###
         ALPHA = 0.0413 * Na + 0.2101                                                        # Relation between ALPHA and Na
         Jmax25 = 117.6 * (Na - 0.17)                                                        # Relation between Jmax25 and Na
-        Jmax = cls._f_temperature('Jmax', Jmax25, Tleaf)                                    # Relation between Jmax and temperature
+        Jmax = cls._f_temperature('Jmax', Jmax25, Torg)                                     # Relation between Jmax and temperature
 
         # Electron transport rate
         J = ((Jmax+ALPHA*PAR) - sqrt((Jmax+ALPHA*PAR)**2 - 4*cls.THETA*ALPHA*PAR*Jmax))/(2*cls.THETA) # Muller et al. (2005), Evers et al. (2010)
         Aj = (J * (Ci-Gamma)) / (4*Ci + 8*Gamma)                                            # Rate of assimilation under RuBP regeneration limitation
         ### Triose phosphate utilisation-limited carboxylation rate ###
         TPU25 = cls.PARAM_N['S_Na']['TPU25'] * (Na - cls.PARAM_N['Na_min']['TPU25'])        # Relation between TPU25 and Na
-        TPU = cls._f_temperature('TPU', TPU25, Tleaf)                                       # Relation between TPU and temperature
+        TPU = cls._f_temperature('TPU', TPU25, Torg)                                        # Relation between TPU and temperature
         Vomax = (Vc_max*Ko*Gamma)/(0.5*Kc*cls.O)                                            # Maximum rate of Vo (umol m-2 s-1)
         Vo = (Vomax * cls.O) / (cls.O + Ko*(1+Ci/Kc))                                       # Rate of oxygenation of RuBP (umol m-2 s-1)
         Ap = (1-Gamma/Ci)*(3*TPU) + Vo                                                      # Rate of assimilation under TPU limitation
@@ -209,9 +221,9 @@ class PhotosynthesisModel(object):
         # Gross assimilation rate
         Ag = min(Ac, Aj, Ap)
 
-        # Mitochondrial respiration rate of leaf in light Rd (processes other than photorespiration)
+        # Mitochondrial respiration rate of organ in light Rd (processes other than photorespiration)
         Rdark25 = cls.PARAM_N['S_Na']['Rdark25'] * (Na - cls.PARAM_N['Na_min']['Rdark25'])  # Relation between Rdark25 (respiration in obscurity at 25 degree C) and Na
-        Rdark = cls._f_temperature('Rdark', Rdark25, Tleaf)                                 # Relation between Rdark and temperature
+        Rdark = cls._f_temperature('Rdark', Rdark25, Torg)                                  # Relation between Rdark and temperature
         Rd = Rdark * (0.33 + (1-0.33)*(0.5)**(PAR/15))                                      # Found in Muller et al. (2005), eq. 19
         # Net C assimilation
         An = Ag - Rd
@@ -219,13 +231,14 @@ class PhotosynthesisModel(object):
 
 
     @classmethod
-    def calculate_An(cls, organ_width, organ_height, PAR, Ta, ambient_CO2, RH, wind0):
+    def calculate_An(cls, organ_width, organ_height, PAR, Ta, ambient_CO2, RH, Ur, organ_name):
         """
         Compute An (µmol m-2 s-1) and Tr (mm s-1).
 
         :Parameters:
 
-            - `organ_width` (:class:`float`) - width of the organ (or diameter for stem organ) (m)
+            - `organ_width` (:class:`float`) - width of the organ (or diameter for stem organ) (m),
+               characteristic dimension to be considered for heat transfer through forced convection (by wind).
 
             - `organ_height` (:class:`float`) - height of the organ from soil (m)
 
@@ -237,7 +250,8 @@ class PhotosynthesisModel(object):
 
             - `RH` (:class:`float`) - relative humidity (decimal fraction)
 
-            - `wind0` (:class:`float`) - wind speed at the top of the canopy (m s-1)
+            - `Ur` (:class:`float`) - Ur: wind at the reference height (zr) (m s-1), e.g. top of the canopy + 2m
+               (in the case of wheat, Ur can be approximated as the wind speed at 2m from soil)
 
         :Returns:
             An (µmol m-2 s-1) and Tr (mm s-1).
@@ -247,32 +261,29 @@ class PhotosynthesisModel(object):
 
         """
         #: Organ parameters
-        LEAF_WIDTH = organ_width                    #: m
         H_CANOPY = 0.8                              #: m
-        H_ORGAN = organ_height                      #: m
         NA_INIT = 2.5                               #: g m-2
 
-        ### Iterations to find leaf temperature and Ci ###
-        Ci, Tleaf = 0.7*ambient_CO2, Ta # Initial values
-        prec_Ci, prec_Tleaf = 0.1, 0.1
+        ### Iterations to find organ temperature and Ci ###
+        Ci, Torg = 0.7*ambient_CO2, Ta # Initial values
+        prec_Ci, prec_Torg = 0.1, 0.1
         count = 0
-        while abs((Ci - prec_Ci)/prec_Ci) >= 0.01 or abs((Tleaf - prec_Tleaf)/prec_Tleaf) >= 0.01:
+        while abs((Ci - prec_Ci)/prec_Ci) >= 0.01 or abs((Torg - prec_Torg)/prec_Torg) >= 0.01:
             if count >=30: # TODO: test a faire? Semble prendre du tps de calcul
                 if abs((Ci - prec_Ci)/prec_Ci) >= 0.01:
                     print 'Ci cannot converge, prec_Ci= {}, Ci= {}'.format(prec_Ci, Ci)
                 else:
-                    print 'Tleaf cannot converge, prec_Tleaf= {}, Tleaf= {}'.format(prec_Tleaf, Tleaf)
+                    print 'Torg cannot converge, prec_Torg= {}, Torg= {}'.format(prec_Torg, Torg)
                 break
             else:
-                prec_Ci, prec_Tleaf = Ci, Tleaf
-                An, Ag, Rd = cls._photosynthesis(PAR, NA_INIT, Tleaf, Ci)
+                prec_Ci, prec_Torg = Ci, Torg
+                An, Ag, Rd = cls._photosynthesis(PAR, NA_INIT, Torg, Ci)
                 # Stomatal conductance
                 gs = cls._stomatal_conductance(Ag, An, NA_INIT, ambient_CO2, RH)
                 # New value of Ci
                 Ci = ambient_CO2 - An * ((1.6/gs) + (1.37/cls.GB)) # gs and GB in mol m-2 s-1
-                # New value of Tleaf
-                Tleaf, Tr = cls._leaf_temperature(LEAF_WIDTH, H_ORGAN, H_CANOPY, wind0, PAR, gs, Ta, Tleaf, RH)
+                # New value of Torg
+                Torg, Tr = cls._organ_temperature(organ_width, organ_height, H_CANOPY, Ur, PAR, gs, Ta, Torg, RH, organ_name)
                 count +=1
 
-        return An, Tr
-
+        return An, Tr, Torg, gs
